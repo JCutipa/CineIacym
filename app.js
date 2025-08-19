2d6fc62a0cbb3021462379502646af98762f271f
Jose Cutipa <92490290+JCutipa@users.noreply.github.com>
2025-08-18 19:50:06 -0500

    Update app.js
    cambio de carpetas

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../frontend/public')));
 
 // Configuración de vistas
-app.set('views', path.join(__dirname, '../frontend/views'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // Conexión a MongoDB
